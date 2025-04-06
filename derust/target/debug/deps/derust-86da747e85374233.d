e69a4319fcd85bec409d416c62f57e36341174bb
Pujith <Pujith@users.noreply.github.com>
2025-04-06 20:17:48 +0530

    Delete derust/target directory

--- derust/target/debug/deps/derust-86da747e85374233.d
+++
@@ -1,5 +0,0 @@
-/Users/valtechadmin/Documents/Personal/Programming/Rust/Rusty/derust/target/debug/deps/derust-86da747e85374233: src/main.rs
-
-/Users/valtechadmin/Documents/Personal/Programming/Rust/Rusty/derust/target/debug/deps/derust-86da747e85374233.d: src/main.rs
-
-src/main.rs:
--- derust/target/debug/derust.d
+++
@@ -1,1 +0,0 @@
-/Users/valtechadmin/Documents/Personal/Programming/Rust/Rusty/derust/target/debug/derust: /Users/valtechadmin/Documents/Personal/Programming/Rust/Rusty/derust/src/main.rs
